c6643d5450214a2dca577528203c3a2e9117ea0c
al-bani <alzildan.abrar.r@gmail.com>
2022-12-21 00:06:01 +0700

    creating pojo.admin & pojo.akun

---
+++ TokoKomputer/src/main/java/Pojo/Admin.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pojo;
+
+/**
+ *
+ * @author alzildan
+ */
+public class Admin {
+    private Integer id;
+    private String nama;
+    private boolean Status;
+    private Akun akun;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public boolean isStatus() {
+        return Status;
+    }
+
+    public void setStatus(boolean Status) {
+        this.Status = Status;
+    }
+
+    public Admin() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Akun getAkun() {
+        return akun;
+    }
+
+    public void setAkun(Akun akun) {
+        this.akun = akun;
+    }
+    
+    
+}
