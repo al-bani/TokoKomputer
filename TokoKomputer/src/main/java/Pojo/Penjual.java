ebfce298c534938b1cc997682cbc504a3b58bb2a
al-bani <alzildan.abrar.r@gmail.com>
2022-12-25 10:21:01 +0700

    add file feature

--- TokoKomputer/src/main/java/Pojo/Penjual.java
+++ TokoKomputer/src/main/java/Pojo/Penjual.java
@@ -9,5 +9,44 @@ package Pojo;
  * @author alzildan
  */
 public class Penjual {
+    private Integer id;
+    private String nama;
+    private boolean Status;
+    private Akun akun;
     
+    public Penjual() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public boolean isStatus() {
+        return Status;
+    }
+
+    public void setStatus(boolean Status) {
+        this.Status = Status;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Akun getAkun() {
+        return akun;
+    }
+
+    public void setAkun(Akun akun) {
+        this.akun = akun;
+    }
 }
