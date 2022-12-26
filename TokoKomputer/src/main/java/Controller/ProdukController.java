5b68ac102847f27157d7814e2659d98aeef65a5c
al-bani <alzildan.abrar.r@gmail.com>
2022-12-26 12:09:20 +0700

    controller produk

--- TokoKomputer/src/main/java/Controller/ProdukController.java
+++ TokoKomputer/src/main/java/Controller/ProdukController.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,23 +28,80 @@ public class ProdukController implements ProdukInterface {
 
     @Override
     public List<Produk> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Produk> listDokter = new ArrayList<>();
+        String sql = "";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            state = conn.createStatement();
+            rs = state.executeQuery(sql);
+            
+            while (rs.next()) {                
+                Produk prod = new Produk();
+                prod.setKode(rs.getString("kode_produk"));
+                prod.setNama(rs.getString("nama_produk"));
+                prod.setPengiriman(rs.getString("pengiriman"));
+                prod.setDeskripsi(rs.getString("deskripsi_produk"));
+                prod.setPembayaran(rs.getString("metode_pembayaran"));
+                prod.setHarga(rs.getDouble("harga_produk"));
+                prod.setStok(rs.getInt("stok_produk"));
+                prod.setBerat(rs.getInt("berat_produk"));
+                
+                listDokter.add(prod);
+            }
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdukController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        
+        return listDokter;
     }
 
     @Override
     public Integer create(Produk object) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int result = 0;
+        String sql = "";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            state = conn.createStatement();
+            state.executeUpdate(sql);
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdukController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return result;  
     }
 
     @Override
     public Integer update(Produk object) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+     int result = 0;
+        String query = "";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            state = conn.createStatement();
+            result = state.executeUpdate(query);
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdukController.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 
     @Override
     public Produk findById(int id) {
        Produk prod = null;
-        String sql = "SELECT * FROM obat WHERE id_obat="+id+"";
+        String sql = "";
         
         conMan = new ConnectionManager();
         conn = conMan.connect();
