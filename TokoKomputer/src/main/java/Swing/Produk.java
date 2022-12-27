56cc11ede5159bedfa36aab79e688c3ea6b129c6
al-bani <alzildan.abrar.r@gmail.com>
2022-12-27 14:24:31 +0700

    produk

--- TokoKomputer/src/main/java/Swing/Produk.java
+++ TokoKomputer/src/main/java/Swing/Produk.java
@@ -8,12 +8,12 @@ package Swing;
  *
  * @author alzildan
  */
-public class Login extends javax.swing.JFrame {
+public class Produk extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login
+     * Creates new form Register
      */
-    public Login() {
+    public Produk() {
         initComponents();
     }
 
@@ -59,20 +59,21 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Produk.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Produk.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Produk.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Produk.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new Produk().setVisible(true);
             }
         });
     }
