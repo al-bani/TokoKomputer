048020218af360ac98669582ff1372fd4a07b372
RamziMubarak <ramzi.mubarak@mhs.itenas.ac.id>
2022-12-27 14:13:57 +0700

    Pesanan Update2

--- TokoKomputer/src/main/java/Swing/pesanan.java
+++ TokoKomputer/src/main/java/Swing/pesanan.java
@@ -6,14 +6,14 @@ package Swing;
 
 /**
  *
- * @author alzildan
+ * @author ramzi
  */
-public class Login extends javax.swing.JFrame {
+public class pesanan extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login
+     * Creates new form pesanan
      */
-    public Login() {
+    public pesanan() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(pesanan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(pesanan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(pesanan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(pesanan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new pesanan().setVisible(true);
             }
         });
     }
