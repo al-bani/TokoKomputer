85fa678d8f27ca2c8c7e5bcb540a9dc6f9ee6ec5
RamziMubarak <ramzi.mubarak@mhs.itenas.ac.id>
2022-12-27 13:38:29 +0700

    pesanan Complite

--- TokoKomputer/src/main/java/Controller/PesananController.java
+++ TokoKomputer/src/main/java/Controller/PesananController.java
@@ -1,13 +1,151 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controller;
 
+import Interface.PesananInterface;
+import Pojo.Pesanan;
+import Utilities.ConnectionManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
- * @author alzildan
+ * @author Ramzi
  */
-public class PesananController {
+
+public class PesananController implements PesananInterface{
+    
+    private ConnectionManager conMan;
+    private Connection conn;
+    Statement stmt;
+    ResultSet rs;
+
+    
+    @Override
+    public List<Pesanan> findAll() {
+        List<Pesanan> listPesanan = new ArrayList<>();
+        String sql = "SELECT * FROM tb_Pesanan";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {                
+                Pesanan pesanan = new Pesanan();
+                pesanan.setId_pesanan(rs.getString("idPesanan"));
+                pesanan.setUsername_pembeli(rs.getString("usernamePembeli"));
+                pesanan.setKode_Produk(rs.getString("kodeProduk"));
+                pesanan.setJumlah_pesanan(rs.getInt("jumlahPesanan"));
+                
+                
+                listPesanan.add(pesanan);
+            }
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(PesananController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        
+        return listPesanan;
+    }
+
+    @Override
+    public Object create(Pesanan object) {
+        int result = 0;
+        String sql = "INSERT INTO tb_produk(id_pesanan, username_pembeli, kode_produk, jumlah_pesanan"
+                + "VALUES('"+object.getId_pesanan()+"', "
+                + ""+object.getUsername_pembeli()+", "
+                + ""+object.getKode_Produk()+", "
+                + "'"+object.getJumlah_pesanan()+"')";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(PesananController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    @Override
+    public Object update(Pesanan object) {
+        int result = 0;
+        String sql = "UPDATE promo SET username_pembeli='"+object.getUsername_pembeli()+"', "
+                + "kode_produk="+object.getKode_Produk()+", "
+                + "jumlah_pesanan="+object.getJumlah_pesanan()+" from tb_pesanan "
+                + "WHERE id_pesanan="+object.getId_pesanan()+"";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            result = stmt.executeUpdate(sql);
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(PesananController.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    @Override
+    public Pesanan findById(int id) {
+        Pesanan pesanan = null;
+        String sql = "SELECT * FROM tb_pesanan WHERE id_pesanan="+id+"";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {
+                pesanan = new Pesanan();
+                pesanan.setId_pesanan(rs.getString("idPesanan"));
+                pesanan.setUsername_pembeli(rs.getString("usernamePembeli"));
+                pesanan.setKode_Produk(rs.getString("kode_produk"));
+                pesanan.setJumlah_pesanan(rs.getInt("jumlahPesanan"));
+            }
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(PesananController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return pesanan;
+    }
+
+    @Override
+    public Object delete(int id) {
+        int result = 0;
+        String query = "DELETE FROM tb_pesanan WHERE id_pesanan"+id+"";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            result = stmt.executeUpdate(query);
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(PesananController.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        
+        return result;
+    }
+    
     
 }
