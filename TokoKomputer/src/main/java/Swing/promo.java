168be8ee3899dbf7a471086e6f88a5ccc0c050ef
RamziMubarak <ramzi.mubarak@mhs.itenas.ac.id>
2022-12-27 14:09:49 +0700

    Promo Update 2

--- TokoKomputer/src/main/java/Swing/promo.java
+++ TokoKomputer/src/main/java/Swing/promo.java
@@ -6,14 +6,14 @@ package Swing;
 
 /**
  *
- * @author alzildan
+ * @author ramzi
  */
-public class Login extends javax.swing.JFrame {
+public class promo extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login
+     * Creates new form promo
      */
-    public Login() {
+    public promo() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(promo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(promo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(promo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(promo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new promo().setVisible(true);
             }
         });
     }
